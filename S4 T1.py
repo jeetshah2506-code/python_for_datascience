01cd5031569339178d54764dede644f5cb63c7dc
jeetshah2506-code <jeetshah2506@gmail.com>
2026-09-22 12:19:39 +0530

    Update S4 T1.py
    Task 3 change

--- S4 T1.py
+++ S4 T1.py
@@ -18,10 +18,11 @@ print(product_name)
 #Task 3: Write a function split_product_code(product_code) that takes a string like 'ZOMATO-FOOD-2024' and returns a list of its parts using the split() method.
 
 def split_product_code(product_code):
-        return product_code.split("-")
-        
+    print(product_code.split("-"))
+
 product_code = "ZOMATO-FOOD-2024"
-print(split_product_code(product_code))
+
+split_product_code(product_code)
 
 
 # Task 4: Given the string 'Spotify_Premium_Offer', use string slicing to extract and print only the word 'Premium'.
